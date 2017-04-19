2be1bcfa8793f6a8ef9d4d01cea50b0cffe244c5
Francisco Saldanha <francisco.gsaldanha@gmail.com>
2017-04-19 16:07:00 -0300

    Document CBMessage

--- src/main/java/com/cheatbreaker/api/message/CBMessage.java
+++ src/main/java/com/cheatbreaker/api/message/CBMessage.java
@@ -4,8 +4,16 @@ import java.util.Map;
 
 public interface CBMessage {
 
+    /**
+     * Messages have actions.
+     * The client knows what to do with the data in {@link #toMap()}
+     * because of the action provided.
+     */
     String getAction();
 
+    /**
+     * The data contained in this Message. Will be sent to the Client.
+     */
     Map<String, Object> toMap();
 
 }
