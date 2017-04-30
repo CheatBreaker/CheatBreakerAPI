2c33aa8a8a0a55795cd49a75ec659e48a1da0e0d
Francisco Saldanha <francisco.gsaldanha@gmail.com>
2017-04-30 12:15:09 -0300

    * Cache players running cheatbreaker when we register the channel to them, remove them from the cache when we unregister them or when they quit. Also resort to that for #isRunningCheatBreaker. * Add #getPlayersRunningCheatBreaker, which will return an ImmutableSet<Player> that has all of the players currently running cheatbreaker.

--- src/main/java/com/cheatbreaker/api/CheatBreakerAPI.java
+++ src/main/java/com/cheatbreaker/api/CheatBreakerAPI.java
@@ -3,23 +3,29 @@ package com.cheatbreaker.api;
 import com.cheatbreaker.api.message.*;
 import com.cheatbreaker.api.object.CBNotification;
 import com.cheatbreaker.api.waypoint.WaypointManager;
+import com.google.common.collect.ImmutableSet;
 import lombok.Getter;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRegisterChannelEvent;
+import org.bukkit.event.player.PlayerUnregisterChannelEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.messaging.Messenger;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public final class CheatBreakerAPI extends JavaPlugin implements Listener {
 
     private static final String MESSAGE_CHANNEL = "CB-Client";
 
     @Getter private static CheatBreakerAPI instance;
+    private final Set<UUID> playersRunningCheatBreaker = new HashSet<>();
 
     @Getter private WaypointManager waypointManager;
 
@@ -37,10 +43,43 @@ public final class CheatBreakerAPI extends JavaPlugin implements Listener {
         messenger.registerIncomingPluginChannel(this, MESSAGE_CHANNEL, (channel, player, bytes) -> {
 
         });
+
+        getServer().getPluginManager().registerEvents(
+                new Listener() {
+
+                    @EventHandler
+                    public void onRegister(PlayerRegisterChannelEvent event) {
+                        if (event.getChannel().equals(MESSAGE_CHANNEL)) {
+                            playersRunningCheatBreaker.add(event.getPlayer().getUniqueId());
+                        }
+                    }
+
+                    @EventHandler
+                    public void onUnregister(PlayerUnregisterChannelEvent event) {
+                        if (event.getChannel().equals(MESSAGE_CHANNEL)) {
+                            playersRunningCheatBreaker.remove(event.getPlayer().getUniqueId());
+                        }
+                    }
+
+                    @EventHandler
+                    public void onUnregister(PlayerQuitEvent event) {
+                        playersRunningCheatBreaker.remove(event.getPlayer().getUniqueId());
+                    }
+
+                }
+        , this);
     }
 
     public boolean isRunningCheatBreaker(Player player) {
-        return player.getListeningPluginChannels().contains(MESSAGE_CHANNEL);
+        return isRunningCheatBreaker(player.getUniqueId());
+    }
+
+    public boolean isRunningCheatBreaker(UUID playerUuid) {
+        return playersRunningCheatBreaker.contains(playerUuid);
+    }
+
+    public Set<Player> getPlayersRunningCheatBreaker() {
+        return ImmutableSet.copyOf(playersRunningCheatBreaker.stream().map(Bukkit::getPlayer).collect(Collectors.toSet()));
     }
 
     public void isCheatBreakerBanned(UUID playerUuid, Consumer<Boolean> resultListener) {
