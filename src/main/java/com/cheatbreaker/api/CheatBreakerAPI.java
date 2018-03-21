b743b2f2e3f8c759a86464b55e01bbc0d596db82
Francisco Saldanha <frgsaldanha@gmail.com>
2018-03-21 16:33:34 -0300

    Add a message to override name-tags for living entities in the client (works for players!)

--- src/main/java/com/cheatbreaker/api/CheatBreakerAPI.java
+++ src/main/java/com/cheatbreaker/api/CheatBreakerAPI.java
@@ -8,6 +8,7 @@ import com.cheatbreaker.api.waypoint.WaypointManager;
 import com.google.common.collect.ImmutableSet;
 import lombok.Getter;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -129,6 +130,18 @@ public final class CheatBreakerAPI extends JavaPlugin implements Listener {
     public void sendTeammates(Player player, TeammatesMessage message) {
         message.validatePlayers(player);
         sendMessage(player, message);
+    }
+
+    public void overrideNametag(LivingEntity target, String nametag, Player viewer) {
+        sendMessage(viewer, new OverrideNametagMessage(target, nametag));
+    }
+
+    public void resetNametag(LivingEntity target, Player viewer) {
+        sendMessage(viewer, new OverrideNametagMessage(target, null));
+    }
+
+    public void hideNametag(LivingEntity target, Player viewer) {
+        sendMessage(viewer, new OverrideNametagMessage(target, ""));
     }
 
     /*
