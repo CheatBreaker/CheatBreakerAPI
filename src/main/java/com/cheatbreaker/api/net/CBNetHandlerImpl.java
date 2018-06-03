cd7a80c40cbd4ada5e7881c3ce422de9941ab9a3
Francisco Saldanha <frgsaldanha@gmail.com>
2018-06-03 18:20:14 -0300

    Remove old method

--- src/main/java/com/cheatbreaker/api/net/CBNetHandlerImpl.java
+++ src/main/java/com/cheatbreaker/api/net/CBNetHandlerImpl.java
@@ -1,13 +1,10 @@
 package com.cheatbreaker.api.net;
 
-import com.cheatbreaker.nethandler.client.CBPacketWaypointUpdate;
 import com.cheatbreaker.nethandler.shared.CBPacketAddWaypoint;
 import com.cheatbreaker.nethandler.shared.CBPacketRemoveWaypoint;
 
 public class CBNetHandlerImpl extends CBNetHandler
 {
-    @Override
-    public void handleWaypointUpdate(CBPacketWaypointUpdate cbPacketWaypointUpdate) {}
 
     @Override
     public void handleAddWaypoint(CBPacketAddWaypoint cbPacketAddWaypoint) {}
